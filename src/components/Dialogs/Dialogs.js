7d49d1dff2e0bb9899aa1ca49f1dc772d210c845
Dima <dima@simply-smart.com>
2020-05-09 22:27:22 +0300

    use data from object in components

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,34 +1,35 @@
 import React from 'react';
-import classes from './Dialogs.module.scss';
 import DialogItem from './Dialog/DialogItem';
+import classes from './Dialogs.module.scss';
 import Message from './Message/Message';
 
 const Dialogs = () => {
+
 	let dialogsData = [
 		{ name: 'Dima', id: 1 },
 		{ name: 'Anna', id: 2 },
 		{ name: 'Rulia', id: 3 },
 		{ name: 'Alina', id: 4 },
 	];
+
+	let dialogsElements = dialogsData.map(dialog => <DialogItem name={dialog.name} id={'/' + dialog.id} />);
+
 	let messagesData = [
 		{ message: 'Hi', id: 1 },
 		{ message: 'Hello', id: 2 },
 		{ message: 'How are you?', id: 3 },
 		{ message: 'OK', id: 4 },
 	];
+
+	let messageElements = messagesData.map(message => <Message message={message.message} id={message.id} />);
+
 	return (
 		<div className={classes.dialogs}>
 			<div className={classes.dialogs__items}>
-				<DialogItem name={dialogsData[0].name} id={'/' + dialogsData[0].id} />
-				<DialogItem name={dialogsData[1].name} id={'/' + dialogsData[1].id} />
-				<DialogItem name={dialogsData[2].name} id={'/' + dialogsData[2].id} />
-				<DialogItem name={dialogsData[3].name} id={'/' + dialogsData[3].id} />
+				{dialogsElements}
 			</div>
 			<div className={classes.dialogs__messages}>
-				<Message message={messagesData[0].message} />
-				<Message message={messagesData[1].message} />
-				<Message message={messagesData[2].message} />
-				<Message message={messagesData[3].message} />
+				{messageElements}
 			</div>
 		</div>
 	)
--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -4,26 +4,28 @@ import Post from '../Post/Post';
 
 
 const MyPosts = () => {
+
 	let postsData = [
 		{ post: 'Hi, what are you doing?', id: 1, likesCount: 1, },
 		{ post: 'I am rest', id: 2, likesCount: 12, },
 		{ post: 'How are you?', id: 3, likesCount: 3, },
 		{ post: 'OK', id: 4, likesCount: 41, },
 	]
+
+	let postItems = postsData.map(postItem => <Post message={postItem.post} likes={postItem.likesCount} />)
 	return (
 		<div className={classes.posts}>
 			<h3>My Posts</h3>
 			<div>
 				<div>
-				<textarea name="" id="" cols="30" rows="10"></textarea>
+					<textarea name="" id="" cols="30" rows="10"></textarea>
 				</div>
 				<div>
-				<button>Add post</button>
+					<button>Add post</button>
 				</div>
 			</div>
 			<div className={classes.post}>
-			<Post message={postsData[0].post} likes={postsData[0].likesCount} />
-			<Post message={postsData[1].post} likes={postsData[1].likesCount} />
+				{postItems}
 			</div>
 		</div>)
 }
