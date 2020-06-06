eccce9fe07642d02dc813d6d4e3d10d45d9330e8
Dima Shlegel <dimashlegel@gmail.com>
2020-06-06 23:12:46 +0300

    add axios - library for work with server API

--- src/components/Users/User/User.js
+++ src/components/Users/User/User.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './User.module.scss';
+import userPlaceholder from '../../../assets/images/user-placeholder.png'
 
 const User = (props) => {
-	// console.log(props);
-	
+
 	return (
 		<div className={styles.user}>
 			<span className={styles.user__left}>
 				<div className={styles.user__ava}>
-					<img src={props.photoUrl} alt="" />
+					<img src={props.photos.small != null ? props.photos.small : userPlaceholder} alt="" />
 				</div>
 				<div className={styles.user__ava}>
 					{props.followed
@@ -19,12 +19,12 @@ const User = (props) => {
 			</span>
 			<span className={styles.user__right}>
 				<span>
-					<div>{props.fullname}</div>
+					<div>{props.name}</div>
 					<div>{props.status}</div>
 				</span>
 				<span>
-					<div>{props.location.country}</div>
-					<div>{props.location.city}</div>
+					<div>{'props.location.country'}</div>
+					<div>{'props.location.city'}</div>
 				</span>
 			</span>
 		</div >
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import User from './User/User';
+import * as axios from 'axios';
 
 const Users = (props) => {
 
@@ -11,18 +12,23 @@ const Users = (props) => {
 		props.unfollow(id);
 	};
 
-	if(props.users.length === 0) {
-		props.setUsers([
-			{ id: 1, followed: true, photoUrl: 'https://www.dovercourt.org/wp-content/uploads/2019/11/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.jpg', fullname: 'Dima', status: 'Ok', location: { country: 'Ukraine', city: 'Kyiv' }, },
-			{ id: 2, followed: true, photoUrl: 'https://www.dovercourt.org/wp-content/uploads/2019/11/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.jpg', fullname: 'Sara', status: 'Mazal Tov', location: { country: 'Israel', city: 'Bat Yam' }, },
-			{ id: 3, followed: false, photoUrl: 'https://www.dovercourt.org/wp-content/uploads/2019/11/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.jpg', fullname: 'Jack', status: 'Protest', location: { country: 'USA', city: 'New York' }, },
-			{ id: 4, followed: false, photoUrl: 'https://www.dovercourt.org/wp-content/uploads/2019/11/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.jpg', fullname: 'Anna', status: 'Wife', location: { country: 'Ukraine', city: 'Kyiv' }, },
-		])
+	if (props.users.length === 0) {
+
+		axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+			props.setUsers(response.data.items);
+		});
+
+		// [
+		// 	{ id: 1, followed: true, photoUrl: 'https://www.dovercourt.org/wp-content/uploads/2019/11/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.jpg', fullname: 'Dima', status: 'Ok', location: { country: 'Ukraine', city: 'Kyiv' }, },
+		// 	{ id: 2, followed: true, photoUrl: 'https://www.dovercourt.org/wp-content/uploads/2019/11/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.jpg', fullname: 'Sara', status: 'Mazal Tov', location: { country: 'Israel', city: 'Bat Yam' }, },
+		// 	{ id: 3, followed: false, photoUrl: 'https://www.dovercourt.org/wp-content/uploads/2019/11/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.jpg', fullname: 'Jack', status: 'Protest', location: { country: 'USA', city: 'New York' }, },
+		// 	{ id: 4, followed: false, photoUrl: 'https://www.dovercourt.org/wp-content/uploads/2019/11/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.jpg', fullname: 'Anna', status: 'Wife', location: { country: 'Ukraine', city: 'Kyiv' }, },
+		// ]
 	}
 
 	let users = props.users.map((user) => (
-		<User key={user.id} photoUrl={user.photoUrl} fullname={user.fullname} followed={user.followed} status={user.status} location={user.location} 
-		follow={follow} unfollow={unfollow} id={user.id} />
+		<User key={user.id} photos={user.photos} name={user.name} followed={user.followed} status={user.status}
+			follow={follow} unfollow={unfollow} id={user.id} />
 	));
 
 
