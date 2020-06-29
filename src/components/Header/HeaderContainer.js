2bb94873311b1ae53fd2c3c112ef8a1c7dfe0411
Dima Shlegel <dimashlegel@gmail.com>
2020-06-29 23:11:00 +0300

    realization follow/unfollow for users

--- src/components/Header/HeaderContainer.js
+++ src/components/Header/HeaderContainer.js
@@ -3,16 +3,27 @@ import Header from "./Header";
 import * as axios from 'axios';
 import { connect } from "react-redux";
 import { toggleIsFetching, setAuthUserData } from "../../redux/auth-reducer";
+// import { setUserProfile } from '../../redux/profile-reducer';
+
 
 class HeaderContainer extends React.Component {
 	componentDidMount() {
+		
 		this.props.toggleIsFetching(true);
+		// { withCredentials: true } -- check for authorization
 		axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, { withCredentials: true }).then(response => {
 			if (response.data.resultCode === 0) {
 				let { id, login, email } = response.data.data;
 				this.props.setAuthUserData(id, email, login);
 			}
 			this.props.toggleIsFetching(false);
+
+			// axios.get(`https://social-network.samuraijs.com/api/1.0//profile/2`, { withCredentials: true }).then(response => {
+			// 	this.props.setUserProfile(response.data);
+			
+			// })
+
+
 		});
 	}
 	render() {
@@ -26,7 +37,9 @@ const mapsStateToProps = (state) => {
 	return ({
 		isAuth: state.auth.isAuth,
 		login: state.auth.login,
+		id: state.auth.userID,
+		// photos: state.profilePage.profile.photos
 	});
 }
-
+// setUserProfile
 export default connect(mapsStateToProps, { toggleIsFetching, setAuthUserData })(HeaderContainer)
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as axios from 'axios';
 import styles from './Users.module.scss';
 import userPlaceholder from '../../assets/images/user-placeholder.png';
 import { NavLink } from 'react-router-dom';
@@ -29,9 +30,32 @@ const Users = (props) => {
 						</div>
 						<div className={styles.user__ava}>
 							{user.followed
-								? <button onClick={() => { props.unfollow(user.id) }}>Unfollow</button>
-								: <button onClick={() => { props.follow(user.id) }}>Follow</button>
-							}
+								? <button onClick={() => {
+									axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`, {
+										withCredentials: true,
+										headers: {
+											"API-KEY": "d2a1ada9-5724-480a-a3e9-feac2410c0d6"
+										},
+									})
+										.then(response => {
+											if (response.data.resultCode === 0) {
+												props.unfollow(user.id)
+											}
+										})
+								}}>Unfollow</button>
+								: <button onClick={() => {
+									axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`, {}, {
+										withCredentials: true,
+										headers: {
+											"API-KEY": "d2a1ada9-5724-480a-a3e9-feac2410c0d6"
+										},
+									})
+										.then(response => {
+											if (response.data.resultCode === 0) {
+												props.follow(user.id)
+											}
+										});
+								}}>Follow</button>}
 						</div>
 					</span>
 					<span className={styles.user__right}>
